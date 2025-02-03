11b492ddbbb2d99cd98934529fa0c74b8cbbdfb4
Andrii Kamenev <Andrii_Kamenev@epam.com>
2025-02-03 11:09:12 +0200

    Service update

--- taxcalculator.client/src/app/work/services/tax-calculator.service.ts
+++ taxcalculator.client/src/app/work/services/tax-calculator.service.ts
@@ -14,15 +14,6 @@ export class TaxService {
   // Use CalculationResult interface for the response
   calculateTax(input: SalaryInput): Observable<CalculationResult> {
     return this.http.post<CalculationResult>(AppConstant.ApiResource.TaxCalculator.GET_CALCULATE_TAX(this.baseUrl), input);
-    //return this.http.post<CalculationResult>("/api/tax/calculate", input);
-    //return of({
-    //  grossAnnualSalary: 30000,
-    //  grossMonthlySalary: 2500,
-    //  netAnnualSalary: 24000,
-    //  netMonthlySalary: 2000,
-    //  annualTaxPaid: 6000,
-    //  monthlyTaxPaid: 500,
-    //});
   }
 }
 
--- taxcalculator.client/src/main.ts
+++ taxcalculator.client/src/main.ts
@@ -2,14 +2,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { APP_INITIALIZER } from '@angular/core';
 
+//  When Angular initializes the app, the BASE_URL is injected using the APP_INITIALIZER provider.
+//  TaxService now receives the BASE_URL dynamically through dependency injection and passes it to AppConstant.GET_CALCULATE_TAX() to form the correct API URL.
+//  This ensures that getBaseUrl() is accessed only after the app is fully initialized, thus preventing the "Cannot access 'TaxService' before initialization" error.
 export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
 }
 // Add a factory to initialize the `BASE_URL` at app startup
 const providers = [
-  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] },
+  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [], multi: false },
   // This will initialize `getBaseUrl()` during the app's bootstrap
-  { provide: APP_INITIALIZER, useFactory: getBaseUrl, deps: [], multi: true,}
+  { provide: APP_INITIALIZER, useFactory: getBaseUrl, deps: [], multi: true}
 ];
 
 platformBrowserDynamic().bootstrapModule(AppModule)
